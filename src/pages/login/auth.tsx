2ab4d852e7598c8ad02af0a7d7c811e6bad9704e
N-Joy-Shadow <shs3740413@gmail.com>
2023-03-27 20:04:53 +0900

    update login page

--- src/pages/login/auth.tsx
+++ src/pages/login/auth.tsx
@@ -4,7 +4,6 @@ import { useLoaderData } from 'react-router-dom';
 export default function Auth() {
     const data =useLoaderData()
     console.log(data)
-
     return (<></>)
 
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,6 @@
 import { McBackground, McBackgroundLayout, McButton } from "../../components/MCStyled/mcStyle";
 import { useMsal } from '@azure/msal-react';
 import { LoginRequest } from "../../utils/auth/authcfg";
-
 export default function Login() {
     const { instance } = useMsal();
 
@@ -11,7 +10,14 @@ export default function Login() {
         })
     }
 
-    return(<McBackground>
+    return(<McBackground className="w-[600px]">
+        <div className="flex gap-2">
+            <img src="./ms_logo.svg" width={64}/>
+            <div>
+                <h1 className="text-center items-center grow text-xl"><strong>로그인</strong></h1><hr/>
+                <p>With Microsoft Account</p>
+            </div>
+        </div>
         <McButton onClick={HandleLogin}>Login With Microsoft Account</McButton>
     </McBackground>)
 };
